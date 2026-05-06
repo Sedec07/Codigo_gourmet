820640edde1598d1b550164f2f1a64606f5a3624
LuciaBermudez <analuciabermudez87@gmail.com>
2026-05-06 15:48:14 +0000

    Estructura inicial del proyecto Codigo Gourmet

---
+++ src/main/java/com/codigo/gourmet/CodigoGourmetApplication.java
@@ -0,0 +1,12 @@
+package com.codigo.gourmet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodigoGourmetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodigoGourmetApplication.class, args);
+    }
+}
